beb1fd923963147f02f8cc33e536093b8c38a710
reduckted <reduckted@outlook.com>
2018-01-20 14:34:48 +1000

    Refactored into a base view model for plots.

--- source/PingViz/Views/LiveView/LiveViewModel.vb
+++ source/PingViz/Views/LiveView/LiveViewModel.vb
@@ -9,11 +9,7 @@ Imports System.Reactive.Linq
 Namespace Views
 
     Public Class LiveViewModel
-        Inherits ViewModelBase
-
-
-        Private Const TimeoutAxisKey As String = "TimeoutAxis"
-        Private Const DurationAxisKey As String = "DurationAxis"
+        Inherits PlotViewModelBase
 
 
         Private Shared ReadOnly Timeframe As TimeSpan = TimeSpan.FromMinutes(3)
@@ -24,10 +20,7 @@ Namespace Views
         Private ReadOnly cgHistoryProvider As IHistoryProvider
         Private ReadOnly cgDateTimeProvider As IDateTimeProvider
         Private ReadOnly cgScheduler As IScheduler
-        Private ReadOnly cgXAxis As Axis
         Private ReadOnly cgCurrent As ObservableAsPropertyHelper(Of Integer?)
-        Private cgSeries As AreaSeries
-        Private cgLastResultTimestamp As Date?
 
 
         Public Sub New(
@@ -60,17 +53,7 @@ Namespace Views
             cgDateTimeProvider = dateTimeProvider
             cgScheduler = scheduler
 
-
-            ' Hold onto the x-axis because we need to update it as time moves forward.
-            ' We don't need to hold on to the y-axis because it doesn't change.
-            cgXAxis = CreateXAxis()
-
-            Plot = New PlotModel
-
-            ConfigurePlot()
-            Plot.Axes.Add(cgXAxis)
-            Plot.Axes.Add(CreateYAxisForDuration())
-            Plot.Axes.Add(CreateYAxisForTimeout())
+            ConfigureTimeAxis()
 
             cgCurrent = resultsSource _
                 .Results _
@@ -81,140 +64,26 @@ Namespace Views
         End Sub
 
 
-        Private Sub ConfigurePlot()
-            Plot.PlotAreaBorderColor = LoadColor("AccentColor")
-            Plot.PlotAreaBorderThickness = New OxyThickness(0, 1, 0, 0)
-            Plot.Padding = New OxyThickness(0)
+        Private Sub ConfigureTimeAxis()
+            TimeAxis.MajorStep = DateTimeAxis.ToDouble(DateTimeAxis.ToDateTime(0).Add(Application.PingInterval))
+
+            SetTimeAxisRange()
         End Sub
 
 
-        Private Function CreateXAxis() As Axis
-            Dim axis As Axis
-
-
-            axis = New DateTimeAxis With {
-                .Position = AxisPosition.Bottom,
-                .IsZoomEnabled = False,
-                .IsPanEnabled = False,
-                .IsAxisVisible = True,
-                .TickStyle = TickStyle.None,
-                .MinimumPadding = 0,
-                .MaximumPadding = 0,
-                .TitleColor = OxyColors.Transparent,
-                .TextColor = LoadColor("GraphTextColor"),
-                .MajorStep = DateTimeAxis.ToDouble(DateTimeAxis.ToDateTime(0).Add(Application.PingInterval)),
-                .AxisTickToLabelDistance = 0,
-                .LabelFormatter = Function(x) String.Empty
-            }
-
-            SetGridLines(axis)
-            SetXAxisRange(axis)
-
-            Return axis
-        End Function
-
-
-        Private Shared Function CreateYAxisForDuration() As Axis
-            Dim axis As Axis
-
-
-            axis = New LinearAxis With {
-                .Key = DurationAxisKey,
-                .Position = AxisPosition.Left,
-                .IsZoomEnabled = False,
-                .IsPanEnabled = False,
-                .TickStyle = TickStyle.None,
-                .Minimum = 0,
-                .MinimumPadding = 0,
-                .AxisTickToLabelDistance = 0,
-                .LabelFormatter = Function(x) String.Empty
-            }
-
-            SetGridLines(axis)
-
-            Return axis
-        End Function
-
-
-        Private Shared Function CreateYAxisForTimeout() As Axis
-            Dim axis As Axis
-
-
-            axis = New LinearAxis With {
-                .Key = TimeoutAxisKey,
-                .Position = AxisPosition.Left,
-                .IsZoomEnabled = False,
-                .IsPanEnabled = False,
-                .IsAxisVisible = False,
-                .Minimum = 0,
-                .Maximum = 1,
-                .MinimumPadding = 0,
-                .MaximumPadding = 0
-            }
-
-            SetGridLines(axis)
-
-            Return axis
-        End Function
-
-
-        Private Shared Function CreateSeries(key As String) As AreaSeries
-            Dim series As AreaSeries
-
-
-            series = New AreaSeries With {.YAxisKey = key}
-
-            If String.Equals(key, DurationAxisKey) Then
-                series.StrokeThickness = 1
-                series.Fill = OxyColor.FromAColor(16, LoadColor("AccentColor"))
-                series.Color = LoadColor("AccentColor")
-            Else
-                series.StrokeThickness = 0
-                series.Fill = LoadColor("TimeoutFillColor")
-                series.Fill = OxyColor.FromUInt32(&H60DB000CUI)
-            End If
-
-            Return series
-        End Function
-
-
-        Private Shared Sub SetGridLines(axis As Axis)
-            axis.MajorGridlineStyle = LineStyle.Solid
-            axis.MajorGridlineColor = LoadColor("Gray9")
-        End Sub
-
-
-        Public Overrides Async Function OnLoadedAsync() As Task
+        Protected Overrides Async Function LoadAsync() As Task
             Dim earliest As Date
             Dim latest As Date
 
 
             ' Load any existing data for the period that we will initially show.
-            latest = cgDateTimeProvider.GetDateTime().RoundDown(Application.PingInterval)
+            latest = DateTimeAxis.ToDateTime(TimeAxis.Maximum).RoundDown(Application.PingInterval)
             earliest = latest.Subtract(Timeframe)
 
-            For Each result In Await cgHistoryProvider.GetPingsAsync(earliest, latest)
-                AddDataPoint(result)
-            Next result
-
-            If cgLastResultTimestamp.HasValue Then
-                Dim nextPossibleTimestamp As Date
-
-
-                ' Given the timestamp of the last result, work out what 
-                ' the earliest timestamp of the next point could be.
-                nextPossibleTimestamp = cgLastResultTimestamp.Value.Add(Application.PingInterval)
-
-                ' If there will definitely be a gap between the last point 
-                ' we added and the first possible live result, then extend
-                ' the last point to where the next point could have been.
-                ' If not, then extend the last point to the end of the graph.
-                If cgLastResultTimestamp.HasValue AndAlso (nextPossibleTimestamp < latest) Then
-                    ExtendLastPoint(nextPossibleTimestamp)
-                Else
-                    ExtendLastPoint(cgXAxis.Maximum)
-                End If
-            End If
+            AddDataPoints(
+                Await cgHistoryProvider.GetPingsAsync(earliest, latest),
+                Application.PingInterval
+            )
 
             ' Now that the graph has been loaded with the
             ' initial data, start watching for new results.
@@ -229,125 +98,13 @@ Namespace Views
                 ).Subscribe(Sub() UpdateGraphForCurrentTime())
             )
 
-            IsLoading = False
+            Plot.InvalidatePlot(True)
         End Function
 
 
         Private Sub AddPingResult(result As PingResult)
-            AddDataPoint(result)
+            AddDataPoint(result, Application.PingInterval)
             Plot.InvalidatePlot(True)
-        End Sub
-
-
-        Private Sub AddDataPoint(result As PingResult)
-            Dim point As DataPoint
-            Dim key As String
-
-
-            ' Work out what type of series the point needs to be put in.
-            key = If(result.Duration.HasValue, DurationAxisKey, TimeoutAxisKey)
-
-            If ShouldStartNewSeries(result, key) Then
-                ' Because we are ending the current series, we need to 
-                ' make sure the point for the last result continues for
-                ' the period that the point represents. So if there is 
-                ' a last result, extend its point by an interval.
-                If cgLastResultTimestamp.HasValue Then
-                    ExtendLastPoint(cgLastResultTimestamp.Value.Add(Application.PingInterval))
-                End If
-
-                cgSeries = CreateSeries(key)
-                Plot.Series.Add(cgSeries)
-            End If
-
-            point = ConvertToDataPoint(result)
-
-            If IsLoading Then
-                ' The initial results are being loaded, which means 
-                ' there isn't an extra point that we need to deal with. 
-                ' We can just add the new point to the end of the list.
-                cgSeries.Points.Add(point)
-
-            ElseIf cgSeries.Points.Count > 0 Then
-                ' We are not loading the initial results, and there are already points
-                ' in the series which means there is an extra point at the end of the list.
-                ' We can replace that extra point with the new point, then add a new extra
-                ' point after it so that the line continues to the edge of the graph.
-                cgSeries.Points(cgSeries.Points.Count - 1) = point
-                cgSeries.Points.Add(New DataPoint(cgXAxis.Maximum, point.Y))
-
-            Else
-                ' We're not loading the initial results, but we are starting a 
-                ' new series, so there isn't an extra point already in the series.
-                ' We can add the new point to the list, then add an extra point
-                ' so that the line continues to the edge of the graph.
-                cgSeries.Points.Add(point)
-                cgSeries.Points.Add(New DataPoint(cgXAxis.Maximum, point.Y))
-            End If
-
-            cgLastResultTimestamp = result.Timestamp
-        End Sub
-
-
-        Private Function ShouldStartNewSeries(
-                result As PingResult,
-                yAxisKey As String
-            ) As Boolean
-
-            ' If there is no current series, then we need to start one.
-            If cgSeries Is Nothing Then
-                Return True
-            End If
-
-            ' If the last result (which which will exist if there's a 
-            ' current series) is more than one interval away from the new 
-            ' result, then the new result needs to be put in a new series.
-            If cgLastResultTimestamp.Value.Add(Application.PingInterval) < result.Timestamp Then
-                Return True
-            End If
-
-            ' If the key for the y-axis that the result belongs to is different
-            ' to that of the current series, then we need to start a new
-            ' series. If not, then it can be added to the same series.
-            Return Not String.Equals(cgSeries.YAxisKey, yAxisKey)
-        End Function
-
-
-        Private Shared Function ConvertToDataPoint(result As PingResult) As DataPoint
-            Dim value As Double
-
-
-            value = result.Duration.ToMilliseconds().GetValueOrDefault(1)
-
-            Return DateTimeAxis.CreateDataPoint(result.Timestamp, value)
-        End Function
-
-
-        Private Sub ExtendLastPoint(extendTo As Date)
-            ExtendLastPoint(DateTimeAxis.ToDouble(extendTo))
-        End Sub
-
-
-        Private Sub ExtendLastPoint(extendTo As Double)
-            If cgSeries IsNot Nothing Then
-                Dim point As DataPoint
-
-
-                point = New DataPoint(extendTo, cgSeries.Points(cgSeries.Points.Count - 1).Y)
-
-                If IsLoading Then
-                    ' The initial results are being loaded, which means 
-                    ' there isn't an extra point in the series. The 
-                    ' last point is the one that we need to extend.
-                    cgSeries.Points.Add(point)
-
-                Else
-                    ' We've finished loading the initial results, which means there
-                    ' is an extra point in the series. That extra point (it's the 
-                    ' last point) can be replaced, rather than adding a new point.
-                    cgSeries.Points(cgSeries.Points.Count - 1) = point
-                End If
-            End If
         End Sub
 
 
@@ -359,17 +116,17 @@ Namespace Views
             ' Check if the very last point in the graph is pinned to 
             ' the maximum value of the x-axis. If it is, then we will 
             ' need to update that point after we change the x-axis.
-            If cgSeries IsNot Nothing Then
-                lastPoint = cgSeries.Points(cgSeries.Points.Count - 1)
-                pinLastPointToMax = (lastPoint.X = cgXAxis.Maximum)
+            If CurrentSeries IsNot Nothing Then
+                lastPoint = CurrentSeries.Points(CurrentSeries.Points.Count - 1)
+                pinLastPointToMax = (lastPoint.X = TimeAxis.Maximum)
             End If
 
-            SetXAxisRange(cgXAxis)
+            SetTimeAxisRange()
 
             ' Update the last point if it's supposed
             ' to be pinned to the edge of the graph.
             If pinLastPointToMax Then
-                cgSeries.Points(cgSeries.Points.Count - 1) = New DataPoint(cgXAxis.Maximum, lastPoint.Y)
+                CurrentSeries.Points(CurrentSeries.Points.Count - 1) = New DataPoint(TimeAxis.Maximum, lastPoint.Y)
             End If
 
             ' Some points may now be below the minimum
@@ -393,7 +150,7 @@ Namespace Views
                 firstPointAboveMin = -1
 
                 For i = 0 To series.Points.Count - 1
-                    If series.Points(i).X > cgXAxis.Minimum Then
+                    If series.Points(i).X > TimeAxis.Minimum Then
                         firstPointAboveMin = i
                         Exit For
                     End If
@@ -465,14 +222,14 @@ Namespace Views
         End Sub
 
 
-        Private Sub SetXAxisRange(axis As Axis)
+        Private Sub SetTimeAxisRange()
             Dim now As Date
 
 
             now = cgDateTimeProvider.GetDateTime().RoundDown(ScrollFrequency)
 
-            axis.Minimum = DateTimeAxis.ToDouble(now.Subtract(Timeframe))
-            axis.Maximum = DateTimeAxis.ToDouble(now)
+            TimeAxis.Minimum = DateTimeAxis.ToDouble(now.Subtract(Timeframe))
+            TimeAxis.Maximum = DateTimeAxis.ToDouble(now)
         End Sub
 
 
@@ -483,42 +240,11 @@ Namespace Views
         End Property
 
 
-        Public ReadOnly Property Plot As PlotModel
-
-
         Public ReadOnly Property Current As Integer?
             Get
                 Return cgCurrent.Value
             End Get
         End Property
-
-
-        Private Shared Function LoadColor(name As String) As OxyColor
-            Dim app As Windows.Application
-
-
-            ' The application and its resources probably
-            ' aren't available while running unit tests.
-            app = Windows.Application.Current
-
-            If app IsNot Nothing Then
-                Dim obj As Object
-
-
-                obj = app.TryFindResource(name)
-
-                If obj IsNot Nothing Then
-                    Dim color As Color
-
-
-                    color = DirectCast(obj, Color)
-
-                    Return OxyColor.FromArgb(color.A, color.R, color.G, color.B)
-                End If
-            End If
-
-            Return Nothing
-        End Function
 
     End Class
 
---
+++ source/PingViz/Views/PlotViewModelBase.vb
@@ -0,0 +1,362 @@
+Imports OxyPlot
+Imports OxyPlot.Axes
+Imports OxyPlot.Series
+Imports System.Reactive.Concurrency
+
+
+Namespace Views
+
+    Public Class PlotViewModelBase
+        Inherits ViewModelBase
+
+
+        Private Const TimeoutAxisKey As String = "TimeoutAxis"
+        Private Const DurationAxisKey As String = "DurationAxis"
+
+
+        Private cgCurrentSeries As AreaSeries
+        Private cgLastResultTimestamp As Date?
+
+
+        Public Sub New(scheduler As IScheduler)
+            MyBase.New(scheduler)
+
+            Plot = New PlotModel With {
+                .PlotAreaBorderColor = LoadColor("AccentColor"),
+                .PlotAreaBorderThickness = New OxyThickness(0, 1, 0, 0),
+                .Padding = New OxyThickness(0)
+            }
+
+            TimeAxis = CreateTimeAxis()
+            DurationAxis = CreateDurationAxis()
+            TimeoutAxis = CreateTimeoutAxis()
+
+            Plot.Axes.Add(TimeAxis)
+            Plot.Axes.Add(DurationAxis)
+            Plot.Axes.Add(TimeoutAxis)
+        End Sub
+
+
+        Private Shared Function CreateTimeAxis() As DateTimeAxis
+            Dim axis As DateTimeAxis
+
+
+            axis = New DateTimeAxis With {
+                .Position = AxisPosition.Bottom,
+                .IsZoomEnabled = False,
+                .IsPanEnabled = False,
+                .MinimumPadding = 0,
+                .MaximumPadding = 0,
+                .AxisTickToLabelDistance = 0,
+                .LabelFormatter = Function(x) String.Empty,
+                .TickStyle = TickStyle.None,
+                .TextColor = LoadColor("Gray5")
+            }
+
+            SetGridLines(axis)
+
+            Return axis
+        End Function
+
+
+        Private Shared Function CreateDurationAxis() As LinearAxis
+            Dim axis As LinearAxis
+
+
+            axis = New LinearAxis With {
+                .Key = DurationAxisKey,
+                .Position = AxisPosition.Left,
+                .IsZoomEnabled = False,
+                .IsPanEnabled = False,
+                .Minimum = 0,
+                .MinimumPadding = 0,
+                .MaximumPadding = 0.1,
+                .AxisTickToLabelDistance = 0,
+                .LabelFormatter = Function(x) String.Empty,
+                .TickStyle = TickStyle.None,
+                .TextColor = LoadColor("Gray5")
+            }
+
+            SetGridLines(axis)
+
+            Return axis
+        End Function
+
+
+        Public Shared Function CreateTimeoutAxis() As Axis
+            Dim axis As LinearAxis
+
+
+            axis = New LinearAxis With {
+                .Key = TimeoutAxisKey,
+                .Position = AxisPosition.Left,
+                .IsZoomEnabled = False,
+                .IsPanEnabled = False,
+                .IsAxisVisible = False,
+                .Minimum = 0,
+                .Maximum = 1,
+                .MinimumPadding = 0,
+                .MaximumPadding = 0
+            }
+
+            Return axis
+        End Function
+
+
+        Private Shared Sub SetGridLines(axis As Axis)
+            axis.MajorGridlineStyle = LineStyle.Solid
+            axis.MajorGridlineColor = LoadColor("Gray10")
+        End Sub
+
+
+        Protected Sub AddDataPoints(
+                results As IEnumerable(Of PingResult),
+                stepSize As TimeSpan
+            )
+
+            Plot.Series.Clear()
+            cgCurrentSeries = Nothing
+            cgLastResultTimestamp = Nothing
+
+            For Each result In results
+                AddDataPoint(result, stepSize)
+            Next result
+
+            If cgLastResultTimestamp.HasValue Then
+                Dim nextPossibleTimestamp As Date
+                Dim latestPointInRange As Date
+
+
+                latestPointInRange = DateTimeAxis.ToDateTime(TimeAxis.Maximum).RoundDown(stepSize)
+
+                ' Given the timestamp of the last result, work out what 
+                ' the earliest timestamp of the next point could be.
+                nextPossibleTimestamp = cgLastResultTimestamp.Value.Add(stepSize)
+
+                ' If there will definitely be a gap between the last point 
+                ' we added and the first possible live result, then extend
+                ' the last point to where the next point could have been.
+                ' If not, then extend the last point to the end of the graph.
+                If cgLastResultTimestamp.HasValue AndAlso (nextPossibleTimestamp < latestPointInRange) Then
+                    ExtendLastPoint(nextPossibleTimestamp)
+                Else
+                    ExtendLastPoint(TimeAxis.Maximum)
+                End If
+            End If
+        End Sub
+
+
+        Protected Sub AddDataPoint(
+                result As PingResult,
+                stepSize As TimeSpan
+            )
+
+            Dim point As DataPoint
+            Dim key As String
+
+
+            ' Work out what type of series the point needs to be put in.
+            key = If(result.Duration.HasValue, DurationAxisKey, TimeoutAxisKey)
+
+            If ShouldStartNewSeries(result, key, stepSize) Then
+                ' Because we are ending the current series, we need to 
+                ' make sure the point for the last result continues for
+                ' the period that the point represents. So if there is 
+                ' a last result, extend its point by an interval.
+                If cgLastResultTimestamp.HasValue Then
+                    ExtendLastPoint(cgLastResultTimestamp.Value.Add(Application.PingInterval))
+                End If
+
+                cgCurrentSeries = CreateSeries(key)
+                Plot.Series.Add(cgCurrentSeries)
+            End If
+
+            point = ConvertToDataPoint(result)
+
+            If IsLoading Then
+                ' The initial results are being loaded, which means 
+                ' there isn't an extra point that we need to deal with. 
+                ' We can just add the new point to the end of the list.
+                cgCurrentSeries.Points.Add(point)
+
+            ElseIf cgCurrentSeries.Points.Count > 0 Then
+                ' We are not loading the initial results, and there are already points
+                ' in the series which means there is an extra point at the end of the list.
+                ' We can replace that extra point with the new point, then add a new extra
+                ' point after it so that the line continues to the edge of the graph.
+                cgCurrentSeries.Points(cgCurrentSeries.Points.Count - 1) = point
+                cgCurrentSeries.Points.Add(New DataPoint(TimeAxis.Maximum, point.Y))
+
+            Else
+                ' We're not loading the initial results, but we are starting a 
+                ' new series, so there isn't an extra point already in the series.
+                ' We can add the new point to the list, then add an extra point
+                ' so that the line continues to the edge of the graph.
+                cgCurrentSeries.Points.Add(point)
+                cgCurrentSeries.Points.Add(New DataPoint(TimeAxis.Maximum, point.Y))
+            End If
+
+            cgLastResultTimestamp = result.Timestamp
+        End Sub
+
+
+        Private Function ShouldStartNewSeries(
+                result As PingResult,
+                yAxisKey As String,
+                stepSize As TimeSpan
+            ) As Boolean
+
+            ' If there is no current series, then we need to start one.
+            If cgCurrentSeries Is Nothing Then
+                Return True
+            End If
+
+            ' If the last result (which which will exist if there's a 
+            ' current series) is more than one interval away from the new 
+            ' result, then the new result needs to be put in a new series.
+            If cgLastResultTimestamp.Value.Add(stepSize) < result.Timestamp Then
+                Return True
+            End If
+
+            ' If the key for the y-axis that the result belongs to is different
+            ' to that of the current series, then we need to start a new
+            ' series. If not, then it can be added to the same series.
+            Return Not String.Equals(cgCurrentSeries.YAxisKey, yAxisKey)
+        End Function
+
+
+        Private Shared Function CreateSeries(key As String) As AreaSeries
+            Dim series As AreaSeries
+
+
+            series = New AreaSeries With {.YAxisKey = key}
+
+            If String.Equals(key, DurationAxisKey) Then
+                series.StrokeThickness = 1
+                series.Fill = OxyColor.FromAColor(16, LoadColor("AccentColor"))
+                series.Color = LoadColor("AccentColor")
+
+            Else
+                series.StrokeThickness = 0
+                series.Fill = OxyColor.FromUInt32(&H60DB000CUI)
+            End If
+
+            Return series
+        End Function
+
+
+        Private Shared Function ConvertToDataPoint(result As PingResult) As DataPoint
+            Dim value As Double
+
+
+            value = result.Duration.ToMilliseconds().GetValueOrDefault(1)
+
+            Return DateTimeAxis.CreateDataPoint(result.Timestamp, value)
+        End Function
+
+
+        Private Sub ExtendLastPoint(extendTo As Date)
+            ExtendLastPoint(DateTimeAxis.ToDouble(extendTo))
+        End Sub
+
+
+        Private Sub ExtendLastPoint(extendTo As Double)
+            If cgCurrentSeries IsNot Nothing Then
+                Dim point As DataPoint
+
+
+                point = New DataPoint(extendTo, cgCurrentSeries.Points(cgCurrentSeries.Points.Count - 1).Y)
+
+                If IsLoading Then
+                    ' The initial results are being loaded, which means 
+                    ' there isn't an extra point in the series. The 
+                    ' last point is the one that we need to extend.
+                    cgCurrentSeries.Points.Add(point)
+
+                Else
+                    ' We've finished loading the initial results, which means there
+                    ' is an extra point in the series. That extra point (it's the 
+                    ' last point) can be replaced, rather than adding a new point.
+                    cgCurrentSeries.Points(cgCurrentSeries.Points.Count - 1) = point
+                End If
+            End If
+        End Sub
+
+
+        Public ReadOnly Property Plot As PlotModel
+
+
+        Protected ReadOnly Property TimeAxis As DateTimeAxis
+
+
+        Protected ReadOnly Property DurationAxis As Axis
+
+
+        Protected ReadOnly Property TimeoutAxis As Axis
+
+
+        Protected ReadOnly Property CurrentSeries As AreaSeries
+            Get
+                Return cgCurrentSeries
+            End Get
+        End Property
+
+
+        Protected Shared Function LoadColor(name As String) As OxyColor
+            Dim app As Windows.Application
+
+
+            ' The application and its resources probably
+            ' aren't available while running unit tests.
+            app = Windows.Application.Current
+
+            If app IsNot Nothing Then
+                Dim obj As Object
+
+
+                obj = app.TryFindResource(name)
+
+                If obj IsNot Nothing Then
+                    Dim color As Color
+
+
+                    color = DirectCast(obj, Color)
+
+                    Return OxyColor.FromArgb(color.A, color.R, color.G, color.B)
+                End If
+            End If
+
+            Return Nothing
+        End Function
+
+
+        Protected Shared Function LoadFont(name As String) As String
+            Dim app As Windows.Application
+
+
+            ' The application and its resources probably
+            ' aren't available while running unit tests.
+            app = Windows.Application.Current
+
+            If app IsNot Nothing Then
+                Dim obj As Object
+
+
+                obj = app.TryFindResource(name)
+
+                If obj IsNot Nothing Then
+                    Dim ff As FontFamily
+
+
+                    ff = DirectCast(obj, FontFamily)
+
+                    Return ff.FamilyNames.First().Value
+                End If
+            End If
+
+            Return Nothing
+        End Function
+
+    End Class
+
+End Namespace
