822a0c783f7312daf566faba549ab7f6e6ce999b
reduckted <reduckted@outlook.com>
2018-01-28 21:22:59 +1000

    Changed mouse controls on the chart.

--- source/PingViz/Views/LiveView/LiveView.xaml.vb
+++ source/PingViz/Views/LiveView/LiveView.xaml.vb
@@ -1,3 +1,4 @@
+Imports OxyPlot
 Imports Prism.Regions
 
 
@@ -6,6 +7,14 @@ Namespace Views
     <Region(Regions.Tabs)>
     <ViewSortHint("0")>
     Public Class LiveView
+
+        Public Sub New()
+            InitializeComponent()
+
+            Chart.Controller = New PlotController
+            Chart.Controller.UnbindAll()
+            Chart.Controller.BindMouseEnter(PlotCommands.HoverPointsOnlyTrack)
+        End Sub
 
     End Class
 
